22b7a434d29ccca1b77c728476ecacaf9d64335f
Haimaimon <105820824+Haimaimon@users.noreply.github.com>
2024-08-23 15:57:38 +0300

    BSPMS249-8

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
-from .views import get_stock_data, predict_stock #, stock_detail_view 
+from .views import get_stock_data, predict_stock  
 
 urlpatterns = [
     path('stock-data/', get_stock_data, name='get_stock_data'),
     path('predict/', predict_stock, name='predict_stock'),
-    #path('stock-detail/<str:ticker>/', stock_detail_view, name='stock_detail_view'),
 ]
 
 
